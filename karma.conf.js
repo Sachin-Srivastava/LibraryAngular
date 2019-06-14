67b88a183672fe5a88f72f65fca8afbf941c0aa8
Sachin Srivastava <ssrivastava@osisoft.com>
2019-06-14 09:45:45 -0400

    moving to service

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular-Workshop'),
+      dir: require('path').join(__dirname, './coverage/Library'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-Workshop'`, () => {
+  it(`should have as title 'Library'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular-Workshop');
+    expect(app.title).toEqual('Library');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Workshop!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Library!');
   });
 });
